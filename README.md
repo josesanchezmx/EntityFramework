ec13551153bd07192f88fd1e3c4db1e412bfa932
josesanchezmx <montieljose711@gmail.com>
2024-03-22 14:13:49 -0600

    Actualizacion del README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Proyecto Entity Framework
 ### Librerias usadas para este proyecto
-# Proyecto Entity Framework
+
 - dotnet add package Humanizer --version 3.0.0-beta.13
 - dotnet paket add Microsoft.EntityFrameworkCore --version 8.0.3
 - dotnet add package Microsoft.EntityFrameworkCore.InMemory --version 8.0.3
--- obj/Debug/net8.0/proyectoef.AssemblyInfo.cs
+++ obj/Debug/net8.0/proyectoef.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoef")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+127c73eb2d35f7af629b41da71ced7eca3737514")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+972ce3db038f8b68949d97ce7bb2c9dc7ddf67d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoef")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoef")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
